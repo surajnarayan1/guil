21b9a5c2729c34acccfdea88ab7035a6b8804f16
Suraj Narayan <snarayan@tavisca.com>
2017-07-23 16:12:02 +0530

    Add project files.

---
+++ Guil/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the  Income");
+            int inc = Convert.ToInt32(Console.ReadLine());
+            CalculateTax taxevaluate = new CalculateTax();
+            taxevaluate.EvaluateTax(inc);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
